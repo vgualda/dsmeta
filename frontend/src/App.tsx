e20540ab9cb6f31fe0af71c31ccfbb4d3f603360
Victor Gualda <vlgualda88@gmail.com>
2022-07-12 01:08:25 -0300

    Primerio componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
-    return(
+  return (
+    <>
       <h1> Olá mundo</h1>
-    )
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  )
 }
 
 export default App
